616ea23be19b0f7bbed12187c25dc19eb96c7578
Zettex <safaandsafa4@gmail.com>
2024-12-04 11:30:10 +0600

    built the interface of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import Letter from './Letter'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
   const [life,setLife] = useState(5)
   const [word ,setWord] = useState(generateRandomWord)
-
+  const letters = []
+  for(let i = 65; i <= 90; i++){
+    letters.push(String.fromCharCode(i))
+  }
+  const letterList = letters.map(x => <Letter value={x}/>)
   function generateRandomWord(){
-    const characters = 'AQWERTYUIOPSDFGHJKLMNBVCXZ';
+    const characters = 'QWERTYUIOPASDFGHJKLMNBVCXZ';
     let randomWord = '';
     for(let i=0;i<5;i++){
       const randomIndex = Math.floor(Math.random()*characters.length)
@@ -20,9 +22,9 @@ function App() {
   return (
     <>
       <div>
-        <h1>Life remaining: {life}</h1>
-        <h3>Word to guess: {word}</h3>
-        
+        <h3>Life remaining: {life}</h3>
+        <h1>Word to guess: {word}</h1>
+        {letterList}
       </div>
     </>
   )
---
+++ src/Letter.jsx
@@ -0,0 +1,5 @@
+export default function Letter(props){
+    return(
+        <button className="letter">{props.value}</button>
+    )
+}
