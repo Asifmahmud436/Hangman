f165568aad182d77f04704e7bb8272a1092ebb83
Zettex <safaandsafa4@gmail.com>
2024-12-04 12:06:20 +0600

    buttons are now objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 import Letter from './Letter'
+import { nanoid } from 'nanoid'
 import './App.css'
 
 function App() {
   const [life,setLife] = useState(5)
-  const [word ,setWord] = useState(generateRandomWord)
-  const letters = []
-  for(let i = 65; i <= 90; i++){
-    letters.push(String.fromCharCode(i))
+  const [word ,setWord] = useState(generateRandomWord())
+  const [letters,setLetters] = useState(createButtons())
+  
+  function createButtons(){
+    let letterArray = []
+    for(let i = 65; i <= 90; i++){
+      let letterElement = {val:String.fromCharCode(i),
+                          isPressed:false,
+                          id:nanoid()
+                        }
+      letterArray.push(letterElement)
+    }
+    return letterArray
   }
-  const letterList = letters.map(x => <Letter value={x}/>)
+  
+  const letterList = letters.map(x => <Letter 
+    key={x.id} 
+    value={x.val} 
+    pressed={x.isPressed}/>)
+
   function generateRandomWord(){
     const characters = 'QWERTYUIOPASDFGHJKLMNBVCXZ';
     let randomWord = '';
@@ -23,7 +38,7 @@ function App() {
     <>
       <div>
         <h3>Life remaining: {life}</h3>
-        <h1>Word to guess: {word}</h1>
+        <h1>{word}</h1>
         <div className='letter-list'>
           {letterList}
         </div>
